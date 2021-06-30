2ed9373875aa4233f52610208bc3baf50a970163
Matt Cline <71410159+clinmatt@users.noreply.github.com>
2021-06-29 23:52:45 -0400

    Added Typescript CDK code to create domain name & certificate

--- src/cdk/typescript/lib/cdk-stack.ts
+++ src/cdk/typescript/lib/cdk-stack.ts
@@ -1,12 +1,20 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as route53 from '@aws-cdk/aws-route53';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as ssm from '@aws-cdk/aws-ssm';
 
 export class ModernTacoShopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const domainNameParameter = new cdk.CfnParameter(this, 'domainName', {
+      type: 'String',
+      description: 'The domain name for the tutorial application.'
+    });
+
 
     // Create a VPC. 
     const vpc = new ec2.Vpc(this, 'vpc', {
@@ -39,5 +47,37 @@ export class ModernTacoShopStack extends cdk.Stack {
     const codeBucketNameOutput = new cdk.CfnOutput(this, 'ModernTacoShop-CodeBucketNameOutput', {
       value: codeBucket.bucketName
     });
+
+
+    // Create the Route 53 hosted zone.
+    const hostedZone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: domainNameParameter.valueAsString
+    });
+
+    // Store the name and ID of the hosted zone in Systems Manager, so we can refer to it in other service scripts.
+    const hostedZoneNameParameter = new ssm.StringParameter(this, 'ModernTacoShop-HostedZoneNameParameter', {
+      parameterName: '/ModernTacoShop/HostedZoneName',
+      stringValue: hostedZone.zoneName,
+      tier: ssm.ParameterTier.STANDARD
+    });
+    const hostedZoneIdParameter = new ssm.StringParameter(this, 'ModernTacoShop-HostedZoneIdParameter', {
+      parameterName: '/ModernTacoShop/HostedZoneID',
+      stringValue: hostedZone.hostedZoneId,
+      tier: ssm.ParameterTier.STANDARD
+    });
+
+    // Register a certificate for the hosted zone.
+    const certificate = new acm.Certificate(this, 'ModernTacoShop-Certificate', {
+      domainName: domainNameParameter.valueAsString,
+      validation: acm.CertificateValidation.fromDns(hostedZone)
+    });
+
+    // Store the certificate ARN in Systems Manager, so we can refer to it in other service scripts.
+    const certificateArnParameter = new ssm.StringParameter(this, 'ModernTacoShop-CertificateArnParameter', {
+      parameterName: '/ModernTacoShop/CertificateARN',
+      stringValue: certificate.certificateArn,
+      tier: ssm.ParameterTier.STANDARD
+    });
+
   }
 }
